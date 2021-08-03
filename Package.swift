04d4fa922a5601ab7acc554d7ecdaf3bb1859462
Nodir Yuldashev <nodir9999999@gmail.com>
2021-08-04 04:49:09 +0500

    Alamofire v up to 5.4.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 //        .package(name: "Alamofire", path: "../Alamofire"),
-        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .branch("master")),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
